46fe26cac64f2498179edc6a8e9fd5ba9b6d62e6
Amgel <angel.albarracin.cv@gmail.com>
2018-10-26 10:16:20 -0300

    Generación automática de login

--- tests/Feature/TestRutas.php
+++ tests/Feature/TestRutas.php
@@ -3,19 +3,21 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
+class TestRutas extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function tesInicio()
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
     }
+
 }
